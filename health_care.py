5dacf622444e6d72d52cf9bf1cd24c7859f5c3b0
Bibix93 <baraa.elalami@gmail.com>
2021-11-02 04:29:47 +0000

    loaded the dataset, input and labels

--- health_care.py
+++ health_care.py
@@ -1,7 +1,17 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import os
 import seaborn as sns
-## comment
+
+import matplotlib.pyplot as plt
+
+import os
 from sklearn.model_selection import train_test_split
+
+## loading the file
+health_care = pd.read_csv('./healthcare/train_data.csv')
+
+## getting the inputs and labels
+
+X = health_care.drop('Stay', axis=1)
+y = health_care['Stay']
+
